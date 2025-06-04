942198c527e65ae341115ea061b97dca546ab147
Ani Carolini Pereira Ribeiro <pereiraribeiroanicarolini02@gmail.com>
2025-06-03 21:39:46 -0300

    Add files via upload

---
+++ prova/scrip.js
@@ -0,0 +1,11 @@
+let valordodinheiro , subtrair1 , subtrair2,valorgasto, valortotal;
+subrtrair1=256.33
+subtrair2=289.18
+valorgasto =  (subtrair1 + subtrair2) 
+valortotal = (valordodinheiro - subtrair1-subtrair2);
+valortotalconvertido = valortotal.toLocaString('pt-br',{
+    style :'currency',
+    currency:'brl'
+
+})
+console.log()
